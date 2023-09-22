ee5c23ad2a9fcb392c9a7bf794a95f98f06f280d
Carlos Silva <128835848+JeanDevv@users.noreply.github.com>
2023-09-22 20:33:09 -0300

    Exemplos práticos da última aula do módulo 2 do Curso de Python Funções apresentadas while True, Break e fstrings

---
+++ aula15.py
@@ -0,0 +1,25 @@
+#1º exemplo
+#cont = 1
+#while True:
+#    print(cont, "-> ", end="")
+#    cont += 1
+#print("Acabou")
+
+#2º exemplo
+#number_user = soma_valores = 0
+#while True:
+#    number_user = int(input("Digite um número: "))
+#    if number_user == 999:
+#        break#função que vai interromper a repetição da estrutura while e vai sair da estrutura e vai executar a linha 15
+#    soma_valores += number_user
+#print("A soma vale {}".format(soma_valores))
+
+#3º exemplo
+#number_user = soma_valores = 0
+#while True:
+#    number_user = int(input("Digite um número: "))
+#    if number_user == 999:
+#        break #função que vai interromper a repetição da estrutura while e vai sair da estrutura e vai executar a linha 15
+#    soma_valores += number_user
+#print("A soma vale {}".format(soma_valores))
+#print(f"A soma vale {soma_valores}") #essa e a nova formatação da função format() 
